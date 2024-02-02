52565dfe490893086d321bf42a1b27bc0788946e
devaharsha-123 <157237142+devaharsha-123@users.noreply.github.com>
2024-02-02 18:08:35 +0530

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,67 +1,20 @@
-function validate(){
-    var password = document.GetElementById('pass');
-    var password = document.GetElementById('upper');
-    var password = document.GetElementById('lower');
-    var password = document.GetElementById('number');
-    var password = document.GetElementById('length');
-    var password = document.GetElementById('sp_char');
-if(pass.value.match(/[0-9]/)){
-    num.style.color='green'
-}
-else{
-    num.style.color='red'
-}
-if(upper.value.match(/[A-Z]/)){
-    upper.style.color='green'
-}
-else{
-    upper.style.color='red'
-}
-if(lower.value.match(/[a-z]/)){
-    lower.style.color='green'
-}
-else{
-    lower.style.color='red'
-}
-if(number.value.match(/[0-9]/)){
-    number.style.color='green'
-}
-else{
-    number.style.color='red'
-}
-if(length.value.match(/[pass value.length>7]/)){
-    length.style.color='green'
-}
-else{
-    length.style.color='red'
-}
-if(sp_char.value.match(/[@\!\#\$\%\^\&\*\?\:\;\.\?\,]/)){
-    sp_char.style.color='green'
-}
-else{
-    sp_char.style.color='red'
-}
-
-
-}
-
-function confirm(){
-    var pass=document.getElementById('pass');
-    var cpass=document.gerElementById('cpass');
-    if(pass.value=cpass.value){
-        document.getElementById('pass').style.display='none';
-        document.getElementById('upper').style.display='none';
-        document.getElementById('lower').style.display='none';
-        document.getElementById('number').style.display='none';
-        document.getElementById('length').style.display='none';
-        document.getElementById('sp_char').style.display='none'
-        }
-    else{
-        document.getElementById('pass').style.display='block';
-        document.getElementById('upper').style.display='block';
-        document.getElementById('lower').style.display='block';            
-        document.getElementById('number').style.display='block';
-        document.getElementById('length').style.display='block';
-        document.getElementById('sp_char').style.display='block';
-        }
-}
+<script>  
+function validateform(){  
+var name=document.myform.name.value;  
+var password=document.myform.password.value;  
+  
+if (name==null || name==""){  
+  alert("Name can't be blank");  
+  return false;  
+}else if(password.length<6){  
+  alert("Password must be at least 6 characters long.");  
+  return false;  
+  }  
+}  
+</script>  
+<body>  
+<form name="myform" method="post" action="abc.jsp" onsubmit="return validateform()" >  
+Name: <input type="text" name="name"><br/>  
+Password: <input type="password" name="password"><br/>  
+<input type="submit" value="register">  
+</form>  
